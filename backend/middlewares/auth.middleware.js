86b585cfeb711aa0543389e5d72e11620b199399
im-vishu <vishantchoudhary2003@gmail.com>
2026-03-09 23:05:32 +0530

    Captain Successfully authenticate

--- backend/middlewares/auth.middleware.js
+++ backend/middlewares/auth.middleware.js
@@ -1,6 +1,8 @@
 const userModel = require('../models/user.model');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+const blacklistTokenModel = require('../models/blacklistToken.model');
+const captainModel = require('../models/captain.model');
 
 
 module.exports.authUser  = async (req, res, next) => {
@@ -27,4 +29,28 @@ module.exports.authUser  = async (req, res, next) => {
                
 }
 
- 
+ module.exports.authCaptain = async (req, res, next) => {
+              const token = req.cookies.token || req.headers.authorization?.split(' ')[1];
+
+              console.log(token);
+
+              if (!token){
+                            return res.status(401).json({message: 'No token provided, authorization denied'});
+              }
+              const isBlacklisted = await blacklistTokenModel.findOne({ token });
+              console.log(isBlacklisted);
+              if (isBlacklisted) {
+                            return res.status(401).json({message: 'Token is blacklisted, authorization denied'});
+              }
+              
+              try {
+                            const decoded = jwt.verify(token, process.env.JWT_SECRET);
+                            const captain = await captainModel.findById(decoded.id);
+                            req.captain = captain;
+                            return next();
+               }
+               catch (error){
+                            console.log(error);
+                            return res.status(401).json({message: 'Invalid token, authorization denied'});
+               }
+}
